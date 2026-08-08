8a15b0fcb6df9809851dd36eccb93c9a1d739ee8
Francis <francis@u.nus.edu>
2026-08-08 15:47:23 +0800

    drip: 5

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,5 +1,12 @@
-import { jsonResponse } from "../lib/http";
+import { badRequest, jsonResponse } from "../lib/http";
+import { isRateLimited } from "../lib/rateLimit";
 
-export function getUser() {
+export function getUser(id?: string) {
+  if (isRateLimited("user")) {
+    return jsonResponse(429, { error: "rate limited" });
+  }
+  if (!id) {
+    return badRequest("id is required");
+  }
   return jsonResponse(200, { user: null });
 }
